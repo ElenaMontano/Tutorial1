712af7782424c1cf6cb3b839b189b8dfd373bd98
ASE-Student <luze.montano@yahoo.de>
2017-11-07 15:51:50 +0100

    Merge branch "hello_user"

---
+++ src/HelloUser.java
@@ -0,0 +1,16 @@
+
+public class HelloUser {
+
+	public HelloUser(String userName) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void greetUser() {
+		String userName = null;
+		// TODO Auto-generated method stub
+		HelloUser hu= new HelloUser(userName);
+		hu.greetUser();
+		
+	}
+
+}
